f2986aa0cee6497abd073a949d47577e3ee7e7d3
DURGASI MAHESH KUMAR <124244425+Mahesh-100@users.noreply.github.com>
2024-04-17 17:54:02 +0530

    updated

--- BankApplication/src/main/java/com/bank/BankApplication/controller/UserController.java
+++ BankApplication/src/main/java/com/bank/BankApplication/controller/UserController.java
@@ -3,7 +3,6 @@ package com.bank.BankApplication.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,36 +28,37 @@ public class UserController {
 	}
 
 
-	@PostMapping("/user")
+	@PostMapping("/register")
 	public UserDTO createUser(@RequestBody UserDTO user) {
 
 		return userService.createUser(user);
 
 	}
 	
-//	 @PostMapping("/login")
-//	    public ResponseEntity<UserDTO> loginUser(@RequestBody UserDTO loginRequest) {
-//	        // Retrieve user from the database based on username
-//		 UserDTO user = userService.getUserByUsername(loginRequest.getUsername());
-//
-//	        if (user != null && user.getPassword().equals(loginRequest.getPassword())) {
-//	            // User found and password matches
-//	            return ResponseEntity.ok(user);
-//	        } else {
-//	            // User not found or password does not match
-//	            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
-//	        }
-//	    }
+	 @PostMapping("/login")
+	    public ResponseEntity<UserDTO> loginUser(@RequestBody UserDTO loginRequest) {
+	        // Retrieve user from the database based on username
+		 UserDTO user = userService.getUserByUsername(loginRequest.getUsername());
+
+	        if (user != null && user.getPassword().equals(loginRequest.getPassword())) {
+	            // User found and password matches
+	            return ResponseEntity.ok(user);
+	        } else {
+	            // User not found or password does not match
+	            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+	        }
+	    }
+	 @GetMapping("/login")
+	    public ResponseEntity<UserDTO> userDetails(@RequestBody UserDTO loginRequest) {
+	        // Retrieve user from the database based on username
+		 UserDTO user = userService.getUserByUsername(loginRequest.getUsername());
+
+		 return ResponseEntity.ok(user);
+	    }
 	
 	
 	
-	@GetMapping("/user")
-    public ResponseEntity<UserDTO> getUserDetails(@AuthenticationPrincipal UserDTO userDetails) {
-        // Retrieve user details using the logged-in username
-        String username = userDetails.getUsername();
-        UserDTO user = userService.getUserByUsername(username);
-        return ResponseEntity.ok(user);
-    }
+
 	
 	@PostMapping("/account")
 	public BankAccountDTO createBankAccount(@RequestBody BankAccountDTO account) {
